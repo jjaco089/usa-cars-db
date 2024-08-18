e4b8a08d220e125f9f9b100298085263d33e1a6a
Jerry Jacob <jacobj2938@gmail.com>
2024-08-17 23:44:02 -0400

    Try using one modify script

---
+++ modify.sql
@@ -0,0 +1,11 @@
+truncate table brand cascade;
+insert into brand values ('Ford', 1903, null);
+insert into brand values ('Chevrolet', 1911, null);
+insert into model values (default, 'Ford', 'Mustang', 1964, null);
+insert into brand values ('Chrysler', 1925, null);
+insert into brand values ('Tesla', 2003, null);
+
+-- 8/17/2024
+insert into brand values ('Lincoln', 1917, null);
+insert into brand values ('Cadillac', 1902, null);
+insert into brand values ('Buick', 1902, null);
